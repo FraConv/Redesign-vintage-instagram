8bebd1d422f0bf3ce285d47900d672bd089e5484
FraConv <francesco16convertini@gmail.com>
2025-10-25 00:59:19 +0200

    Added home page and update main file

---
+++ lib/screens/home_instagram.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeInstagram extends StatefulWidget {
+  const HomeInstagram({super.key});
+
+  @override
+  State<HomeInstagram> createState() => _HomeInstagramState();
+}
+
+class _HomeInstagramState extends State<HomeInstagram> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
